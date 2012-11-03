92c69cb0af90e942a61e494f3b568069094593c9
Andrew E. Slaughter <andrew.e.slaughter@gmail.com>
2012-11-03 13:18:07 -0400

    Documented System class

--- +mFEM/System.m
+++ +mFEM/System.m
@@ -1,22 +1,27 @@
 classdef System < mFEM.handle_hide
     %SYSTEM A class for automatic assembly of finite element equations.
+    % This class allows the specification of the finite element equations
+    % for matrices and vectors as strings, the assembly is handled
+    % automatically.
     %
-    % Syntax
+    % Example
+    %   mesh = FEmesh()
+    %   mesh.grid('Line2',0,1,10)
+    %   mesh.init()
+    %
     %   sys = System(mesh)
-    %   sys = System(mesh, 'PropertyName', PropertyValue)
+    %   sys.add_constant('k',10);
+    %   sys.add_matrix('K','B''*k*B');
+    %   K = sys.assemble('K');
+    %   full(K)
     %
-    % Description
-    %   sys = System(mesh) creates a System object based on the supplied
-    %       FEmesh object.
-    %   sys = System(mesh, 'PropertyName', PropertyValue) same as above but
-    %       allows the options to be changes
-    %
-    % System Properties Descriptions
-    %   Time
-    %       true | {false}
-    %       A toggle for displaying the matrix and vector assembly time.
-    %   
     % See Also FEMESH
+    %
+    %----------------------------------------------------------------------
+    % Copyright 2012 Andrew E. Slaughter
+    % This software is for educational purposes only and may not be used
+    % without written permession.
+    %----------------------------------------------------------------------
     
     properties (SetAccess = private, GetAccess = public)
         mesh = mFEM.FEmesh.empty;   % mesh object
@@ -25,15 +30,38 @@ classdef System < mFEM.handle_hide
     end
     
     properties(Access = private)
-        reserved = {'N','B','x','y','z','xi','eta','zeta'};
-        mat = struct('name', char, 'eqn', char, 'func', char, 'matrix',sparse([]),'boundary_id', uint32([]));
-        vec = struct('name', char, 'eqn' ,char, 'func', char, 'vector',[],'boundary_id', uint32([]));
-        const = struct('name', char, 'value',[]);
+        reserved = ... % reserved variables
+            {'N','B','x','y','z','xi','eta','zeta'};
+        mat = ... % matrix storage structure
+            struct('name', char, 'eqn', char, 'func', char, 'matrix',sparse([]),'boundary_id', uint32([]));
+        vec = ... % vector storage structure
+            struct('name', char, 'eqn' ,char, 'func', char, 'vector',[],'boundary_id', uint32([]));
+        const = ... % constant storage structure
+            struct('name', char, 'value',[]);
     end
     
     methods (Access = public)
         function obj = System(mesh, varargin)
-            %SYSTEM Class constructor.
+            %SYSTEM Class constructor.    
+            %
+            % Syntax
+            %   sys = System(mesh)
+            %   sys = System(mesh, 'PropertyName', PropertyValue)
+            %
+            % Description
+            %   sys = System(mesh) creates a System object based on the
+            %   supplied FEmesh object.
+            %
+            %   sys = System(mesh, 'PropertyName', PropertyValue) same as 
+            %   above but allows the options to be changes
+            %
+            % System Properties Descriptions
+            %   Time
+            %       true | {false}
+            %       A toggle for displaying the matrix and vector 
+            %       assembly time.
+            %   
+            % See Also FEMESH
             
             % Parse the user-defined options
             obj.opt = gather_user_options(obj.opt, varargin{:});
@@ -69,23 +97,69 @@ classdef System < mFEM.handle_hide
         end
         
         function add_matrix(obj, name, eqn, varargin)  
-            %ADD_MATRIX Create a sparse finite element matrix
+            %ADD_MATRIX Create a sparse finite element matrix for assembly
+            %
+            % Syntax
+            %   add_matrix('MatrixName', MatrixEqn)
+            %   add_matrix('MatrixName', MatrixEqn, id)
+            %
+            % Description
+            %   add_matrix('MatrixName', MatrixEqn) adds
+            %   a matrix and the associated equation for finite element
+            %   assembly. The MatrixEqn is a string that is valid MATLAB
+            %   (i.e., it works with the eval function) that gives the 
+            %   integrands of the finite element equations at the element
+            %   level. The variable N is predefined as the element shape 
+            %   functions vector and B is pre-defined as grad(N). Any
+            %   constant added with the ADD_CONSTANT method may be used.
+            %
+            %   add_matrix('MatrixName', MatrixEqn, id) same as above, but
+            %   the equation is only applied on the boundaries with the
+            %   same id (see FEMESH.ADD_BOUNDARY)
+            %
+            % Examples
+            %   sys.add_matrix('M','N''*N');
+            %   sys.add_matrix('K','B''*B');
             
             % Storate location in matrix array
             idx = length(obj.mat) + 1;
             
+            % Add to the matrix structure
             obj.mat(idx).name = name;
             obj.mat(idx).eqn = eqn;
             obj.mat(idx).func = obj.parse_equation(eqn);
-%             obj.mat(idx).matrix = sparse(obj.mesh.n_dof, obj.mesh.n_dof);
-            obj.mat(idx).matrix = sparse([]);
+            obj.mat(idx).matrix = mFEM.Matrix.empty;
             obj.mat(idx).boundary_id = varargin{:};
         end
 
         function add_vector(obj, name, eqn, varargin)  
-            %ADD_VECTOR Create a finite element vector
+            %ADD_VECTOR Create a finite element vector for assembly
+            %
+            % Syntax
+            %   add_vector('VectorName', VectorEqn)
+            %   add_vector('VectorName', VectorEqn, id)
+            %
+            % Description
+            %   add_vector('VectorName', VectorEqn) adds
+            %   a vector and the associated equation for finite element
+            %   assembly. The VectorEqn is a string that is valid MATLAB
+            %   (i.e., it works with the eval function) that gives the 
+            %   integrands of the finite element equations at the element
+            %   level. The variable N is predefined as the element shape 
+            %   functions vector and B is pre-defined as grad(N). Any
+            %   constant added with the ADD_CONSTANT method may be used.
+            %
+            %   add_vector('VectorName', VectorEqn, id) same as above, but
+            %   the equation is only applied on the boundaries with the
+            %   same id (see FEMESH.ADD_BOUNDARY)
+            %
+            % Example
+            %   sys.add_vector('f','N''*b'); % b is a constant
             
+            % Storage location for the vector
             idx = length(obj.vec) + 1;
+            
+            % Append the vector to the structure
             obj.vec(idx).name = name;
             obj.vec(idx).eqn = eqn;
             obj.vec(idx).func = obj.parse_equation(eqn);
@@ -94,10 +168,20 @@ classdef System < mFEM.handle_hide
         end
         
         function X = get(obj, name)
-            %GET Returns the value for the specified name
+            %GET Returns the value for the specified name.
+            %
+            % Syntax
+            %   X = get(name)
+            %
+            % Description
+            %   X = get(name) returns the value of constant, vector, or 
+            %   matrix given the string name. In the case of of matrices
+            %   the unassemble Matrix class object is returned.
             
+            % Determine the type and location in the structure
             [type, idx] = obj.locate(name);
             
+            % Extract the value based on the type
             switch type;
                 case 'matrix'; X = obj.mat(idx).matrix;
                 case 'vector'; X = obj.vet(idx).vector;
@@ -107,9 +191,22 @@ classdef System < mFEM.handle_hide
         
         function X = assemble(obj, name)
             %ASSEMBLE Assembles matrix or vector given by name
+            %
+            % Syntax
+            %   X = assemble(name)
+            %
+            % Description
+            %   X = assemble(name) assembles the finite element matrix or
+            %   vector. In the case of a matrix the sparse matrix is
+            %   returned, for vectors a tradional MATLAB array is given.
 
             % Locate the matrix or vector
             [type, idx] = obj.locate(name);
+            
+            % Error if called on something other than a matrix or vector
+            if any(strcmpi(type,{'matrix','vector'});
+                error('System:Assemble','No assembly routine for %s types', type);
+            end
             
             % Display a time message
             if obj.opt.time;
@@ -120,8 +217,6 @@ classdef System < mFEM.handle_hide
             switch lower(type);
                case 'matrix'; X = obj.assemble_matrix(idx); 
                case 'vector'; X = obj.assemble_vector(idx);
-               otherwise
-                   error('System:Assemble','No assembly routine for %s types', type);
             end
             
             % End message
@@ -135,6 +230,14 @@ classdef System < mFEM.handle_hide
         
         function [type, idx] = locate(obj, name)
             %LOCATE Returns the type and index for the supplied name
+            % 
+            % Syntax
+            %   [type,idx] = locate(name)
+            %
+            % Description
+            %   [type,idx] = locate(name) given the string name, the type
+            %   ('matrix','vector','constant') and the index within the
+            %   storage structure that the name exists.
             
             % Initialize variables
             idx = [];                       % location of name
@@ -164,6 +267,13 @@ classdef System < mFEM.handle_hide
         
         function add_const(obj, name, value)
             %ADD_CONST Adds a single constant to the system
+            %
+            % Syntax
+            %   add_const(name,value)
+            %
+            % Description
+            %   add_const(name,value) adds a single constant to the System, 
+            %   see ADD_CONSTANT for details.
             
             % Test that the constant is not reserved
             if any(strcmp(name,obj.reserved));
@@ -186,16 +296,28 @@ classdef System < mFEM.handle_hide
         function fcn = parse_equation(obj, eqn, varargin)
             %PARSE_EQUATION Converts given equation into a useable function
             %
-            % Syntax:
+            % Syntax
             %   parse_equation(eqn)
-            %   parse_equation(eqn,'side');
-            
+            %   parse_equation(eqn,'-side')
+            %
+            % Description
+            %   parse_equation(eqn) given an equation string, as detailed
+            %   in ADD_MATRIX and ADD_VECTOR, create a working an equation
+            %   for use in the assembly routines.
+            %
+            %   parse_equation(eqn,'-side') same as above but builds the
+            %   equation for use as a side integral (lowers the dimension)
+
             % Get the dimensions of the FE space (use the local, it is what
             % determines the number of xi,eta,... inputs)
             n_dim = obj.mesh.local_n_dim;
             
             % Adjust for special side case
-            if nargin == 3 && strcmpi(varargin{1},'side');
+            options.side = false;
+            options = gather_user_options(options, varargin{:});
+            
+            %  Adjust the dimension for side case
+            if options.side;
                 n_dim = n_dim - 1;
             end
 
@@ -229,6 +351,14 @@ classdef System < mFEM.handle_hide
         
         function eqn = apply_constants(obj, eqn)
             %APPLY_CONSTANTS applies constants to a string equation
+            %
+            % Syntax
+            %   eqn = apply_constant(eqn)
+            %
+            % Description
+            %   eqn = apply_constant(eqn) given the eqn string (see 
+            %   ADD_MATRIX and ADD_VECTOR) and applies any constants that
+            %   were given via ADD_CONSTANT.
                         
             % Loop through each constant and add if present
             for i = 1:length(obj.const);
@@ -256,7 +386,14 @@ classdef System < mFEM.handle_hide
         end
         
         function K = assemble_matrix(obj, idx)
-            %ASSEMBLE_VECTOR Assembles a vector for given index
+            %ASSEMBLE_MATRIX Assembles a matrix for given index.
+            %
+            % Syntax
+            %   K = assemble_matrix(idx)
+            %
+            % Description
+            %   K = assemble_matrix(idx) assembles the desired matrix,
+            %   where idx is the location in the mat structure.
 
             % Clear existing matrix
             obj.mat(idx).matrix = sparse(obj.mesh.n_dof, obj.mesh.n_dof);
@@ -276,9 +413,16 @@ classdef System < mFEM.handle_hide
         
         function assemble_matrix_side(obj, idx)
             %ASSEMBLE_MATRIX_SIDE computes matrix equation on boundary
+            %
+            % Syntax
+            %   K = assemble_matrix_side(idx)
+            %
+            % Description
+            %   K = assemble_matrix_side(idx) assembles the desired matrix 
+            %   for a side where idx is the location in the mat structure.
            
             % Build the function for the side
-            fcn = str2func(obj.parse_equation(obj.mat(idx).eqn, 'side'));
+            fcn = str2func(obj.parse_equation(obj.mat(idx).eqn, '-side'));
 
             % Extract the boundary ids
             id = obj.mat(idx).boundary_id;
@@ -344,6 +488,14 @@ classdef System < mFEM.handle_hide
         
         function assemble_matrix_elem(obj, idx)
             %ASSEMBLE_MATRIX_ELEM Assemble a matrix for entire domain
+            %
+            % Syntax
+            %   K = assemble_matrix_elem(idx)
+            %
+            % Description
+            %   K = assemble_matrix_elem(idx) assembles the desired matrix 
+            %   over the entire domain where idx is the location in the 
+            %   mat structure.
 
             % Create function from the function string
             fcn = str2func(obj.mat(idx).func);
@@ -383,6 +535,13 @@ classdef System < mFEM.handle_hide
         
         function f = assemble_vector(obj, idx)
             %ASSEMBLE_VECTOR Assembles a vector for given index
+            %
+            % Syntax
+            %   K = assemble_vector(idx)
+            %
+            % Description
+            %   K = assemble_vector(idx) assembles the desired vector,
+            %   where idx is the location in the vec structure.
             
             % Clear existing vector
             obj.vec(idx).vector = zeros(obj.mesh.n_dof, 1);
@@ -401,10 +560,17 @@ classdef System < mFEM.handle_hide
         end  
         
         function assemble_vector_side(obj, idx)
-            %ASSEMBLE_VECTOR_SIDE computes vector equation on boundary
+            %ASSEMBLE_VECTOR_SIDE computes vector equation on boundary 
+            %
+            % Syntax
+            %   K = assemble_vector_side(idx)
+            %
+            % Description
+            %   K = assemble_vector_side(idx) assembles the desired vector 
+            %   for a side where idx is the location in the vec structure.   
             
             % Build the function for the side
-            fcn = str2func(obj.parse_equation(obj.vec(idx).eqn, 'side'));
+            fcn = str2func(obj.parse_equation(obj.vec(idx).eqn, '-side'));
             
             % Extract the boundary ids
             id = obj.vec(idx).boundary_id;
@@ -463,6 +629,14 @@ classdef System < mFEM.handle_hide
         
         function assemble_vector_elem(obj, idx)
             %ASSEMBLE_VECTOR_ELEM Assebles a vector across entire domain 
+            %
+            % Syntax
+            %   K = assemble_vector_elem(idx)
+            %
+            % Description
+            %   K = assemble_matrix_elem(idx) assembles the desired matrix 
+            %   over the entire domain where idx is the location in the 
+            %   vec structure.     
             
             % Create the function for element calculations
             fcn = str2func(obj.vec(idx).func);
