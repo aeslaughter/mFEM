2c31c5074d92ce64943ccaaf7aa4b58ae45cfd78
Andrew E. Slaughter <andrew.e.slaughter@gmail.com>
2012-12-11 22:35:33 -0500

    Added example using LinearSolver; added code for listing example problems on html page

---
+++ doc/list_of_examples.m
@@ -0,0 +1,12 @@
+%% List of Examples
+% The following page contains a complete list and brief summary of the
+% example problems included with the mFEM Toolbox.
+%
+%% Manual Assembly Examples
+% * <example1a.html Example 1a: Example 5.1 of Fish & Belytschko (2007)>
+%
+%% Automatic Assembly Examples
+%
+%
+%% References
+%
