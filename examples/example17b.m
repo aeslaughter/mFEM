11b99c7757b96e8cddd0e9595ebfc93e06fa31e0
Andrew E. Slaughter <andrew.e.slaughter@gmail.com>
2013-01-16 11:23:08 -0500

    Still playing with example17b

--- examples/example17b.m
+++ examples/example17b.m
@@ -47,7 +47,7 @@ mesh.plot(ue,'-new');
 
 function u = exact(x)
 [theta,r] = cart2pol(x(:,1),x(:,2));
-% if theta < 0; theta = theta + 2*pi(); end
+% if theta< 0; theta = theta + 2*pi(); end
 u = r.^(2/3).*sin(2/3*theta);
 
 
