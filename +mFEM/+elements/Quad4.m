aec1f2c125636c19a9208d6aaa7d852896579086
Andrew E. Slaughter <andrew.e.slaughter@gmail.com>
2013-02-18 17:25:44 -0500

    Working on building nodes in parallel for Quad4

--- +mFEM/+elements/Quad4.m
+++ +mFEM/+elements/Quad4.m
@@ -91,57 +91,159 @@ classdef Quad4 < mFEM.cells.base.Cell
     
     methods (Static, Access = ?mFEM.Mesh)
         function [nodes,elements] = grid(x0,x1,y0,y1,xn,yn)
-            
-            x = x0 : (x1-x0)/xn : x1;
-            y = y0 : (y1-y0)/yn : y1;
 
-            % Loop through the grid, creating elements for each cell
-            id = 0;
-            n = 0;
+            spmd
+                id = 0;
+                n_nodes = 0;
+                N = zeros(4,xn*yn);
 
-            N = zeros(xn*yn,4);
-            nodes = cell(length(x)*length(y),1); size(nodes)
-            elements = cell(xn*yn,1);
-% nodes(1,:) = [x(i), y(j)];
-% nodes(2,:) = [x(i+1), y(j)];
-% nodes(3,:) = [x(i+1), y(j+1)];
-% nodes(4,:) = [x(i), y(j+1)];
-            for i = 1:xn;
-                for j = 1:yn;
-                    id = id + 1;
-
-                    if i == 1 && j == 1;
-                        nodes{n+1} = mFEM.elements.base.Node(n+1,[x(i),y(j)]);
-                        nodes{n+2} = mFEM.elements.base.Node(n+2,[x(i+1), y(j)]);
-                        nodes{n+3} = mFEM.elements.base.Node(n+3,[x(i+1), y(j+1)]);
-                        nodes{n+4} = mFEM.elements.base.Node(n+4,[x(i), y(j+1)]);             
-                        N(id,:) = n+1:n+4;
-                        n = n + 4;
-                    elseif i == 1 && j > 1;
-                        b = id-1;
-                        nodes{n+1} = mFEM.elements.base.Node(n+1,[x(i+1), y(j+1)]);
-                        nodes{n+2} = mFEM.elements.base.Node(n+2,[x(i), y(j+1)]);  
-                        N(id,:) = [N(b,4), N(b,3), n+1, n+2];
-                        n = n + 2;
-                    elseif i > 1 && j == 1;
-                        l = id-yn;
-                        nodes{n+1} = mFEM.elements.base.Node(n+1,[x(i+1), y(j)]);
-                        nodes{n+2} = mFEM.elements.base.Node(n+2,[x(i+1), y(j+1)]);
-                        N(id,:) = [N(l,2), n+1, n+2, N(l,3)];
-                        n = n + 2;
-                    else
-                        b = id-1;
-                        l = id-yn;
-                        nodes{n+1} = mFEM.elements.base.Node(n+1,[x(i+1), y(j+1)]); 
-                        N(id,:) = [N(l,2), N(b,3), n+1, N(l,3)];
-                        n = n + 1;
+                for i = 1:xn;
+                    for j = 1:yn;
+                        id = id + 1;
+                        if i == 1 && j == 1;
+                            N(:,id) = n_nodes+1:n_nodes+4;
+                            n_nodes = n_nodes + 4;
+                        elseif i == 1 && j > 1;
+                            b = id-1; 
+                            N(:,id) = [N(b,4), N(3,b), n_nodes+1, n_nodes+2];
+                            n_nodes = n_nodes + 2;
+                        elseif i > 1 && j == 1;
+                            l = id-yn;
+                            N(:,id) = [N(2,l), n_nodes+1, n_nodes+2, N(3,l)];
+                            n_nodes = n_nodes + 2;
+                        else
+                            b = id-1;
+                            l = id-yn;
+                            N(:,id) = [N(2,l), N(3,b), n_nodes+1, N(3,l)];
+                            n_nodes = n_nodes + 1;
+                        end
                     end
-                    
-                    elements{id} = mFEM.elements.Quad4(id,nodes(N(id,:)));
                 end
+                
+                N = codistributed(N);
             end
+              
+            spmd
+                X = codistributed(x0 : (x1-x0)/xn : x1);
+                Y = codistributed(y0 : (y1-y0)/yn : y1);
+               
+                x = getLocalPart(X);
+                y = getLocalPart(Y);
+                
+
+                local = cell(length(x)*length(y),1);
+                k = 0;
+                for i = 1:length(x);
+                    for j = 1:length(y);
+                        k = k + 1;
+%                         local{k} = mFEM.elements.base.Node([x(i),y(j)]);
+                    end
+                end
+                
+                codist = codistributor1d(1,codistributor1d.unsetPartition, [n_nodes,1]);
+                nodes = codistributed.build(local,codist);
+            end
+
             
-            size(nodes)
+            
+%           nodes = {};
+          elements = {};
+            
+%           spmd
+%               N_local = getLocalPart(N);
+%               id_local = globalIndices(N,2);
+%               codist = getCodistributor(N);
+%               id = cumsum(codist.Partition);
+% 
+%                 for i = 1:xn;
+%                     id = id + 1;
+% 
+%                     if i == 1 && j == 1;
+%                        nodes{n+1} = mFEM.elements.base.Node(n+1,[x(i),y(j)]);
+%                        nodes{n+2} = mFEM.elements.base.Node(n+2,[x(i+1), y(j)]);
+%                        nodes{n+3} = mFEM.elements.base.Node(n+3,[x(i+1), y(j+1)]);
+%                        nodes{n+4} = mFEM.elements.base.Node(n+4,[x(i), y(j+1)]);             
+%                         N(id,:) = n+1:n+4;
+%                         n = n + 4;
+%                     elseif i == 1 && j > 1;
+%                         b = id-1;
+%                        nodes(n+1) = {mFEM.elements.base.Node(n+1,[x(i+1), y(j+1)])};
+%                        nodes(n+2) = {mFEM.elements.base.Node(n+2,[x(i), y(j+1)])};  
+%                         N(id,:) = [N(b,4), N(b,3), n+1, n+2];
+%                         n = n + 2;
+%                     elseif i > 1 && j == 1;
+%                         l = id-yn;
+%                        nodes(n+1) = {mFEM.elements.base.Node(n+1,[x(i+1), y(j)])};
+%                        nodes(n+2) = {mFEM.elements.base.Node(n+2,[x(i+1), y(j+1)])};
+%                         N(id,:) = [N(l,2), n+1, n+2, N(l,3)];
+%                         n = n + 2;
+%                     else
+%                         b = id-1;
+%                         l = id-yn;
+%                         nodes(n+1) = {mFEM.elements.base.Node(n+1,[x(i+1), y(j+1)])}; 
+%                         N(id,:) = [N(l,2), N(b,3), n+1, N(l,3)];
+%                         n = n + 1;
+%                     end
+% 
+%                     nodes = codistributed(nodes);
+%                     elements{id} = mFEM.elements.Quad4(id,nodes(N(id,:)));
+%                 end
+%           end
+          
+          
+          
+          
+          
+          
+%             x = x0 : (x1-x0)/xn : x1;
+%             y = y0 : (y1-y0)/yn : y1;
+% 
+% %             spmd
+%             
+%                 % Loop through the grid, creating elements for each cell
+%                 id = 0;
+%                 n = 0;
+% 
+%                 N = zeros(xn*yn,4);
+%                 nodes = cell(length(x)*length(y),1); 
+%                 elements = cell(xn*yn,1);
+% 
+%                 for i = 1:xn;
+%                     for j = 1:yn;
+%                         id = id + 1;
+%                         
+%                         if i == 1 && j == 1;
+%                            % nodes{n+1} = mFEM.elements.base.Node(n+1,[x(i),y(j)]);
+%                            % nodes{n+2} = mFEM.elements.base.Node(n+2,[x(i+1), y(j)]);
+%                            % nodes{n+3} = mFEM.elements.base.Node(n+3,[x(i+1), y(j+1)]);
+%                            % nodes{n+4} = mFEM.elements.base.Node(n+4,[x(i), y(j+1)]);             
+%                             N(id,:) = n+1:n+4;
+%                             n = n + 4;
+%                         elseif i == 1 && j > 1;
+%                             b = id-1;
+%                            % nodes(n+1) = {mFEM.elements.base.Node(n+1,[x(i+1), y(j+1)])};
+%                            % nodes(n+2) = {mFEM.elements.base.Node(n+2,[x(i), y(j+1)])};  
+%                             N(id,:) = [N(b,4), N(b,3), n+1, n+2];
+%                             n = n + 2;
+%                         elseif i > 1 && j == 1;
+%                             l = id-yn;
+%                            % nodes(n+1) = {mFEM.elements.base.Node(n+1,[x(i+1), y(j)])};
+%                            % nodes(n+2) = {mFEM.elements.base.Node(n+2,[x(i+1), y(j+1)])};
+%                             N(id,:) = [N(l,2), n+1, n+2, N(l,3)];
+%                             n = n + 2;
+%                         else
+%                             b = id-1;
+%                             l = id-yn;
+%                             %nodes(n+1) = {mFEM.elements.base.Node(n+1,[x(i+1), y(j+1)])}; 
+%                             N(id,:) = [N(l,2), N(b,3), n+1, N(l,3)];
+%                             n = n + 1;
+%                         end
+% 
+%                         %nodes = codistributed(nodes);
+%                         %elements{id} = mFEM.elements.Quad4(id,nodes(N(id,:)));
+%                     end
+%                 end
+%             end
         end
     end    
 end
